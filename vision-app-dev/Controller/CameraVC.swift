9ed4d94b1c430356d754c3d2098e5053f242dd29
Mamo <m.abeli@t4project.com>
2018-09-29 22:44:15 +0200

    Camera View

--- vision-app-dev/Controller/CameraVC.swift
+++ vision-app-dev/Controller/CameraVC.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraVC: UIViewController {
 
+    var captureSession: AVCaptureSession!
+    var captureOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
     @IBOutlet weak var captureImageView: UIImageView!
     @IBOutlet weak var flashBtn: UIButton!
     @IBOutlet weak var recognizedObjLbl: UILabel!
@@ -21,7 +26,41 @@ class CameraVC: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        previewLayer.frame = cameraView.bounds
+    }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+        
+        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            if captureSession.canAddInput(input) == true {
+                captureSession.addInput(input)
+            }
+            
+            captureOutput = AVCapturePhotoOutput()
+            if captureSession.canAddOutput(captureOutput) == true {
+                captureSession.addOutput(captureOutput)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+                previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+                
+                cameraView.layer.addSublayer(previewLayer)
+                captureSession.startRunning()
+            }
+        } catch {
+            debugPrint(error)
+        }
+    }
 
 }
 
